15de002c0a028dbcd76cd860e8a61eb441bbf4ce
Akash Singhal <singhlakash1234@gmail.com>
2022-07-20 10:59:32 +0530

    first commit

---
+++ 1.py
@@ -0,0 +1,7 @@
+#WAP to multiple three number and get the input from user
+
+n1 = int(input("enter the first number"))
+n2 = int(input("enter the second number"))
+n3 = int(input("enter the thrid number"))
+result = n1*n2*n3
+print("result after multiple three is",result);
